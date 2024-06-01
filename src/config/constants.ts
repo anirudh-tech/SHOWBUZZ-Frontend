f54bffcf38038a7ab6fd307d564cccd6d472c1d0
Anirudh-tech <anirudhjagath2002@gmail.com>
2024-06-01 12:31:22 +0530

    refactor: baseurl updated

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:8000'
+export const baseUrl = 'k8s-default-ingressc-12c40cb339-516497939.us-east-1.elb.amazonaws.com'
 export const SOCKET_URL = import.meta.env.VITE_REACT_APP_SOCKET_URL
