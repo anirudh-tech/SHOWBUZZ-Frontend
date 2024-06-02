d9ac509a386ae38df31c76cab020bae4cffe0701
Anirudh-tech <anirudhjagath2002@gmail.com>
2024-06-02 18:12:19 +0530

    baseUrl changed

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'k8s-default-ingressc-12c40cb339-516497939.us-east-1.elb.amazonaws.com'
+export const baseUrl = 'k8s-default-showbuzz-0d76b96f50-1561540104.us-east-1.elb.amazonaws.com'
 export const SOCKET_URL = import.meta.env.VITE_REACT_APP_SOCKET_URL
