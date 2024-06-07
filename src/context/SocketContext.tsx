7243b8c6cfd64ac08e3027b7f6ffe21581204773
Anirudh-tech <anirudhjagath2002@gmail.com>
2024-06-07 15:52:44 +0530

    fix: package.json

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -32,7 +32,7 @@ export const SocketProvider = ({ children }: any) => {
 
   useEffect(() => {
     if(user){
-      const newSocket = io("showbuzz.tickertick.shop/chat",{
+      const newSocket = io("showbuzz.tickertick.shop",{
         query:{
           userId: id
         }
