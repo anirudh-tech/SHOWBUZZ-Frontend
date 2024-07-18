3ed8dde20b1ab61a43f179c2e80a7ea94225d80d
anirudh-tech <anirudhjagath2002@gmail.com>
2024-07-18 17:46:05 +0530

    fix: Backend Url changed

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://showbuzz.tickertick.shop'
+export const baseUrl = 'https://showbuzz-api-gateway-latest.onrender.com'
 export const SOCKET_URL = import.meta.env.VITE_REACT_APP_SOCKET_URL
